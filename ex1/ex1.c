5d204c9897a68154fe628f2b16a11974de86431b
Brooks <bpoltl1@gmail.com>
2019-05-01 15:13:33 -0500

    finished ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,5 +1,7 @@
-// Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
-// (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
+// Write a program that calls `fork()`. Before calling `fork()`, 
+//have the main process access a variable
+// (e.g., x) and set its value to something (e.g., 100). 
+//What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
 
 #include <stdio.h>
@@ -9,6 +11,16 @@
 int main(void)
 {
     // Your code here
-
+    int number;
+    pid_t pid = fork();
+    if(pid == 0){
+        printf("I am the child: %d\n", (int) getpid());
+        number = 200;
+    }
+    else if (pid > 0){
+        printf("I am the parent: %d\n", (int) getpid());
+        number = 100;
+    }
+    printf("the number is : %d \n", number);
     return 0;
 }
