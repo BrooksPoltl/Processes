2d7f8d3c221898df1bb1842b1d1cb7b43128b62f
brookspoltl <bpoltl1@gmail.com>
2019-05-02 15:01:20 -0500

    completed mvp

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -21,6 +21,18 @@ and `clock_gettime()` should work just fine.
 int main()
 {
     // Your code here
-    
+    int diff;
+    struct timespec start, end;
+    long average = 0;
+    int index = 0;
+    while (index != number_iter){
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        write(fileno(stdout), NULL, 0);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+        index = index + 1;
+        average = (average + diff)/index;
+    }
+	printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
     return 0;
 }
